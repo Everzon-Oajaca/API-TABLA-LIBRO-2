fbb63ec59fc4ecdc6928de7088edec93b46257ed
Everzon-Oajaca <eoajacac@miumg.edu.gt>
2024-08-25 19:24:18 -0600

    Add files via upload

---
+++ app/config/env.js
@@ -0,0 +1,17 @@
+
+
+const env = {
+  database: 'umg_salama_2456',
+  username: 'umg_salama_2456_user',
+  password: 'RfMn69acCmgdnkVI1fOa439IeglU15Xo',
+  host: 'dpg-cqin552j1k6c739hjtv0-a',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
---
+++ app/models/libros.model.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, Sequelize) => {
+    const Book = sequelize.define('book', {
+      numero: {
+        type: Sequelize.INTEGER, // Autonumérico
+        autoIncrement: true,
+        primaryKey: true
+      },
+      codigoDelLibro: {
+        type: Sequelize.INTEGER, // Código del libro, numérico byte
+        allowNull: false
+      },
+      codigoDelUsuario: {
+        type: Sequelize.INTEGER, // Código del usuario, numérico byte
+        allowNull: false
+      },
+      fechaDeSalida: {
+        type: Sequelize.DATE, // Fecha de salida, fecha/hora mediana
+        allowNull: false
+      },
+      fechaMaximaParaDevolver: {
+        type: Sequelize.DATE, // Fecha máxima para devolver, fecha/hora mediana
+        allowNull: false
+      },
+      fechaDeDevolucion: {
+        type: Sequelize.DATE, // Fecha de devolución, fecha/hora mediana
+      }
+    });
+  
+    return Book;
+  }
+  
